49de382bb09c198652a24ee308aa67ce8dd5508c
biothomme <55461592+biothomme@users.noreply.github.com>
2021-12-12 19:53:57 +0100

    init; makes pagebuilding more convenient

---
+++ scripts/build_pages.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# build all pages
+python3 title_page.py
+python3 right_placement_pages.py
+python3 error_page.py
+python3 placement_pages.py
+python3 bird_pages.py 
+python3 start_placement_page.py
+python3 phylogenetics_page.py
