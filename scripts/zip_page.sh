c01216e79fe186e6be7f479711e7393def5d1796
biothomme <55461592+biothomme@users.noreply.github.com>
2022-01-09 16:02:13 +0100

    change

--- scripts/zip_page.sh
+++ scripts/zip_page.sh
@@ -9,20 +9,21 @@ PDIR=$1
 for FL in `ls ../data | grep -v 'images_'`
 do
 	[[ ! -d "${PDIR}/data/" ]] && mkdir "${PDIR}/data/"
-	cp -r "../data/${FL}" "${PDIR}/data/${FL}"	
+	cp -r "../data/${FL}" "${PDIR}/data"	
 done
 
 for FL in `ls ../data/images_std | grep -v 'raw' | grep -v 'csv'`
 do
 	[[ ! -d "${PDIR}/data/images_std" ]] && mkdir "${PDIR}/data/images_std"
-	cp -r "../data/images_std/${FL}" "${PDIR}/data/images_std/${FL}"	
+	cp -r "../data/images_std/${FL}" "${PDIR}/data/images_std"	
 done
 
+[[ ! -d "${PDIR}/meta" ]] && mkdir "${PDIR}/meta"
+[[ ! -d "${PDIR}/public" ]] && mkdir "${PDIR}/public"
+cp -r ../meta/* "${PDIR}/meta/"
+cp -r ../public/* "${PDIR}/public/"
 
-cp -r ../meta "${PDIR}/meta"
-cp -r ../public "${PDIR}/public"
-
-zip -r -Z bzip2 "${PDIR}.zip" ${PDIR}
+# zip -r -Z bzip2 "${PDIR}.zip" ${PDIR}
 
 # rm -r ${PDIR}
 echo "Webpage was exported to ${PDIR}."
