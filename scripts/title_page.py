593c74224a1410066bc61575d81584eb8746fe15
biothomme <55461592+biothomme@users.noreply.github.com>
2022-05-16 11:41:37 +0200

    rephrased clip info

--- scripts/title_page.py
+++ scripts/title_page.py
@@ -141,17 +141,7 @@ class TitlePage(AbstractPage):
         else :
             p("Το μοριακό εργαστήριο έλαβε δείγμα πουλιού από αεροπλάνο"
               " που πρέπει να αναγνωριστεί. Μπορείτε να τους βοηθήσετε;")
-        
-        # sequence video
-        if self.lang == "EN":
-            p("Where do the DNA samples come from? Watch this short movie!")
-        else:
-            p("Από πού προέρχονται τα δείγματα DNA; Δες  αυτή τη μικρή ταινία!")
-        raw('<iframe width="560" height="315" src="https://www.youtube.com/embed/jXPf-nHdoxo" '
-                'title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; '
-                'clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
-
-            
+         
         sp_abspath = StartPlacementPage(language=self.lang, stop_html_init=True).make_page_path()
         sp_path = os.path.relpath(sp_abspath, os.path.dirname(self.make_page_path()))
         with form():
@@ -166,6 +156,14 @@ class TitlePage(AbstractPage):
                     value="Γίνε ένας πραγματικός ερευνητής πουλιών",                                
                     onclick=f"window.location.href='{sp_path}'") 
                 
+        # sequence video
+        if self.lang == "EN":
+            p("Where do the DNA samples come from? Watch this short movie!")
+        else:
+            p("Από πού προέρχονται τα δείγματα DNA; Δες  αυτή τη μικρή ταινία!")
+        raw('<iframe width="560" height="315" src="https://www.youtube.com/embed/jXPf-nHdoxo" '
+                'title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; '
+                'clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
         return
 
 
